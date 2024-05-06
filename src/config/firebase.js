5d39ee887a12e1e761752589e186faa613368b5a
Waris Ahmed <warisahmed2022@gmail.com>
2024-05-06 13:22:12 +0500

    first commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,95 +1,96 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,
-onAuthStateChanged, GoogleAuthProvider , signInWithPopup } from "firebase/auth";
+import {
+  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,
+  onAuthStateChanged, GoogleAuthProvider, signInWithPopup
+} from "firebase/auth";
 import Swal from 'sweetalert2'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEEag4gBVwi9ddllT2pp03xEAfgsdE8Lw",
-  authDomain: "client-work-e3d0f.firebaseapp.com",
-  projectId: "client-work-e3d0f",
-  storageBucket: "client-work-e3d0f.appspot.com",
-  messagingSenderId: "804144365354",
-  appId: "1:804144365354:web:306c16f0de77c5ab656853",
-  measurementId: "G-HXDNYTTXFH"
+  apiKey: "AIzaSyD195F2zoBDofAcRCDuCpFuPCsiunl_Toc",
+  authDomain: "data-echo-prod.firebaseapp.com",
+  projectId: "data-echo-prod",
+  storageBucket: "data-echo-prod.appspot.com",
+  messagingSenderId: "22393559285",
+  appId: "1:22393559285:web:d549056dd6de31a27d4dbc"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export async function userSignUp (data) {
-  const { email , password } = data
+export async function userSignUp(data) {
+  const { email, password } = data
 
- await createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    console.log('user' , user);
-    Swal.fire({
-      title: "Good job!",
-      text: "Register Successfull!",
-      icon: "success"
+  await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log('user', user);
+      Swal.fire({
+        title: "Good job!",
+        text: "Register Successfull!",
+        icon: "success"
+      });
+      setTimeout(() => {
+        window.location.href = ('signin')
+      }, 2000);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error.message);
+      // ..
     });
-    setTimeout(() => {
-      window.location.href = ('signin')
-    }, 2000);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(error.message);
-    // ..
-  });
 }
 
-export async function userSignIn (data) {
-  const { email , password } = data
+export async function userSignIn(data) {
+  const { email, password } = data
   await signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    Swal.fire({
-      title: "Good job!",
-      text: "Sign In Successfull!",
-      icon: "success"
-    });
-    setTimeout(() => {
-      window.location.href = ('/')
-    }, 2000);
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      Swal.fire({
+        title: "Good job!",
+        text: "Sign In Successfull!",
+        icon: "success"
+      });
+      setTimeout(() => {
+        window.location.href = ('/')
+      }, 2000);
 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    if(errorCode == 'auth/invalid-credential'){
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode == 'auth/invalid-credential') {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "User Not Found Sign Up First!",
+        });
+        return
+      }
       Swal.fire({
         icon: "error",
         title: "Oops...",
-        text: "User Not Found Sign Up First!",
+        text: "Something Went Wrong Try Again!",
       });
       return
-    }
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Something Went Wrong Try Again!",
     });
-    return
-  });
 }
 
-export async function userSignInWithGoogle () {
-  try{
+export async function userSignInWithGoogle() {
+  try {
     await signInWithPopup(auth, provider)
     window.location.href = ('/')
-  }catch(error){
-    console.error('Error Sign In With Google',error)
+  } catch (error) {
+    console.error('Error Sign In With Google', error)
   }
 }
 
-export async function usersignOut () {
-  try{
+export async function usersignOut() {
+  try {
     await signOut(auth)
     Swal.fire({
       title: "Good job!",
@@ -99,9 +100,9 @@ export async function usersignOut () {
     setTimeout(() => {
       window.location.href = ('/')
     }, 2000);
-  }catch(e){
+  } catch (e) {
     alert(e.message)
   }
 }
 
-export{auth , onAuthStateChanged}
+export { auth, onAuthStateChanged }
